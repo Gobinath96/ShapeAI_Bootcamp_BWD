f58e9301f3f5ddb1236ebc1d5d01ddca2da0a84c
Gobinath96 <86592016+Gobinath96@users.noreply.github.com>
2021-06-28 13:11:49 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Basic Web Development</h1>
+      <p>A web development on React.js using html and css</p>
+    </div>
+  );
+}
+
+export default Info;
